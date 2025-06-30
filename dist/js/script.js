866d59036ebae4371c26a65812c0f361f8c66fab
Valerii Hud <support@valerii-hud.com>
2025-06-30 15:46:08 +0200

    feat: added timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,6 +3,7 @@
   !*** ./src/js/main.js ***!
   \************************/
 window.addEventListener('DOMContentLoaded', () => {
+  // TABS
   const tabsParent = document.querySelector('.tabheader__items'),
     tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent');
@@ -33,6 +34,48 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+  // TIMER
+
+  const deadline = '2025-07-02';
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(total / (1000 * 60 * 60 * 24)),
+      hours = Math.floor(total / (1000 * 60 * 60) % 24),
+      minutes = Math.floor(total / 1000 / 60 % 60),
+      seconds = Math.floor(total / 1000 % 60);
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timerInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock() {
+      const total = getTimeRemaining(endtime);
+      days.textContent = getZero(total.days);
+      hours.textContent = getZero(total.hours);
+      minutes.textContent = getZero(total.minutes);
+      seconds.textContent = getZero(total.seconds);
+      if (!total.total || total.total <= 0) clearInterval(updateClock);
+    }
+  }
+  setClock('.timer', deadline);
 });
 /******/ })()
 ;
